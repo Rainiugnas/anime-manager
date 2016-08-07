5eb8ed56dbbda919505dd546f5b17f77e39b8d44
Pierre Guego <Rainiugnas@Rains-MacBook-Pro.local>
2016-08-07 15:35:51 -0700

    Add link to create / back / admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_back?
+    %w(new edit).include?(action_name) && %w(animes).include?(controller_name)
+  end
+
+  def display_create_anime?
+    %w(new edit).exclude?(action_name) && %w(animes).include?(controller_name)
+  end
 end
