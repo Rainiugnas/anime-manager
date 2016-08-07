ea443344c5fb05083f548862f952be71e11119e2
Pierre Guego <Rainiugnas@Rains-MacBook-Pro.local>
2016-08-07 11:49:06 -0700

    Add anime controller + stats / root path

---
+++ app/controllers/anime_controller.rb
@@ -0,0 +1,11 @@
+class AnimeController < ApplicationController
+
+  ## Render stats (homepage)
+  # Access:
+  #   Route: GET /
+  #   Helper: root_path
+  #   Format: Html
+  ##
+  def stats
+  end
+end
