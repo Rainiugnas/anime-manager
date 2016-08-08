3eaf1379a7f638be9037331dbfcba88a7e23ddfe
Pierre Guego <Rainiugnas@Rains-MacBook-Pro.local>
2016-08-07 20:19:19 -0700

    v2-release - finsh release model

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,2 +1,17 @@
 class Release < ActiveRecord::Base
+  #Constants
+  def self.quarters() %w(winter spring summer fall) end
+
+  #validates
+  validates :quarter, inclusion: { in: Release.quarters }
+  validates :quarter, uniqueness: { scope: :year }
+  validates :year, presence: true
+
+  #Default value
+  ## Set date in function of quarter and year
+  after_validation do
+    month = Release.quarters.find_index(self.quarter) * 3 + 1
+
+    self.date = Date.new self.year, month, 1
+  end
 end
